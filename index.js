fea49c838174ab8a6d83ee6e2dc73776c68e37aa
donghui <ldhmisu@gmail.com>
2019-12-24 17:59:04 +0800

    拆分分发socket的结构

---
+++ index.js
@@ -0,0 +1,5 @@
+const browser = require('./lib/browser');
+const manager = require('./lib/manager');
+const pm2client = require('./lib/pm2client');
+
+module.exports = {browser, manager, pm2client};
