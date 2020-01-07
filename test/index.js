52547722ee9154a3ebd7291b4df6da263f57b19c
donghui <ldhmisu@gmail.com>
2020-01-07 17:36:20 +0800

    添加router，修改开启脚本

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const manager = require('../lib/manager');
 
 function main () {
-    manager.startServer({pm2ips: [ '10.10.3.64' ]});
+    manager.startServer({pm2clients: [ '10.10.3.64' ]});
 }
 
 main();
